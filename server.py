af6d5416e959deea18ffda3f05a5bac68bdd6aff
Joseph Bellahcen <joeclb@icloud.com>
2021-02-27 18:09:05 -0500

    Update server config

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import jsonify
 import connexion
 
+from api.Vectorizer import Vectorizer
+from api.PasswordClassifier import PasswordClassifier
 
 # Create the application instance
 app = connexion.App(__name__, specification_dir="./")
@@ -12,10 +14,11 @@ app.add_api("main.yaml")
 # create a URL route in our application for "/"
 @app.route("/")
 def home():
-    msg = {"msg": "It's working!"}
+    msg = {"msg": "Server active!"}
     return jsonify(msg)
 
 
 if __name__ == "__main__":
     # localhost:8080 for Docker
     app.run(host="0.0.0.0", port=8080, debug=True)
+
